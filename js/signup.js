9667c8022173f431f0a2f2aa83908ab6200f8ed2
Tushar Saxena <tushar.dec6@gmail.com>
2024-10-19 01:30:23 +0530

    Created aunthantiction in signup form

--- js/signup.js
+++ js/signup.js
@@ -1,21 +1,51 @@
+
 document.getElementById('submit-btn').addEventListener('click', function (event) {
     event.preventDefault();
-    let data = {
-        "username": document.getElementById('username').value,
-        "rollNum": document.getElementById('rollno').value,
-        "phoneNum": document.getElementById('phoneNo').value,
-        "email": document.getElementById('email').value,
-        "password": document.getElementById('password').value,
-        "hostelName": document.getElementById('hostelName').value,
-        "floorNum": document.getElementById('floorNum').value,
-        "roomNum": document.getElementById('roomNum').value,
-        "broadCast": [],
-        "marketPlace": [],
-        "logined": 1,
-    }
+    let username = document.getElementById('username').value
+    let rollNum = document.getElementById('rollno').value
+    let phoneNum = document.getElementById('phoneNo').value
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    let hostelName = document.getElementById('hostelName').value
+    let floorNum = document.getElementById('floorNum').value
+    let roomNum = document.getElementById('roomNum').value
 
-    localStorage.setItem(localStorage.length, JSON.stringify(data))
-    window.location = '../html/explore.html'
+    if (username.length < 4) {
+        alert("Username can't be less than 4")
+    }
+    else if (rollNum.length != 10) {
+        alert("Invalid roll number")
+    }
+    else if (phoneNum.length != 10) {
+        alert("Invalid contact number")
+    }
+    else if (password.length < 4) {
+        alert("Passoword can't be less than 4")
+    }
+    else if (floorNum.length != 1) { 
+        alert("Floor number should contain only single character")
+    }
+    else if (roomNum.length != 3) {
+        alert("Room number should contain only 3 characters")
+     }
+    else {
+        let data = {
+            "username": username,
+            "rollNum": rollNum,
+            "phoneNum": phoneNum,
+            "email": email,
+            "password": password,
+            "hostelName": hostelName,
+            "floorNum": floorNum,
+            "roomNum": roomNum,
+            "broadCast": [],
+            "marketPlace": [],
+            "logined": 1,
+        }
+
+        localStorage.setItem(localStorage.length, JSON.stringify(data))
+        window.location = '../html/explore.html'
+    }
 })
 
 document.getElementById('logo').addEventListener('click', function () {
